a5fd8d7087a0c1e89b74dca905503cce3cc8030e
Andrés Sancho <asancho.95.tr@gmail.com>
2026-04-15 01:35:21 +0200

    feat: hosting conf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA77mAuCsp0msL_EusCbyNzs57CmMzwhqA",
+    authDomain: "tarot-app-de86.firebaseapp.com",
+    projectId: "tarot-app-de86",
+    storageBucket: "tarot-app-de86.firebasestorage.app",
+    messagingSenderId: "1023775078468",
+    appId: "1:1023775078468:web:6c004944c6f5edeac6e411"
+  },
+  baseUrl: 'http://localhost:3000/api',
+  theme: 'default',
+};
